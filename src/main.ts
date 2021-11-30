1c7b90373f846df0b82613c072c23bdabd1ab807
Ender <endernull@icloud.com>
2021-11-30 17:30:48 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle(process.env.npm_package_name)
     .setDescription(process.env.npm_package_description)

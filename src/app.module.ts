532cf708fd32e012911be70a2ede4c6a8728fb9f
Ender <endernull@icloud.com>
2021-12-01 04:48:01 +0100

    add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ZaragozaController } from './controllers/zaragoza.controller';
 import { ZineController } from './controllers/zine.controller';
@@ -7,12 +8,17 @@ import { ZineService } from './services/zine.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV
+        ? `.env.${process.env.NODE_ENV}`
+        : '.env',
+    }),
     ClientsModule.register([
       {
         name: 'ZARAGOZA_SERVICE',
         transport: Transport.TCP,
         options: {
-          host: 'canopus-zaragoza',
+          host: process.env.ZARAGOZA_SERVICE_HOST,
           port: 8877,
         },
       },
@@ -20,7 +26,7 @@ import { ZineService } from './services/zine.service';
         name: 'ZINE_SERVICE',
         transport: Transport.TCP,
         options: {
-          host: 'canopus-zine',
+          host: process.env.ZINE_SERVICE_HOST,
           port: 8878,
         },
       },
--- src/models/zine.interface.ts
+++ src/models/zine.interface.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class Session {
   @ApiProperty()
+  date: string;
+
+  @ApiProperty()
   time: string;
 
   @ApiProperty()
